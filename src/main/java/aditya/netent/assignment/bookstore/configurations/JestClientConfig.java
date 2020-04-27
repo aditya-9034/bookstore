a17e2d68a65b9e920a39c7d4a129b965b66cf41c
aditya-9034 <sharmaditya123.as@outlook.com>
2020-04-28 02:05:45 +0530

    put es host in config

--- src/main/java/aditya/netent/assignment/bookstore/configurations/JestClientConfig.java
+++ src/main/java/aditya/netent/assignment/bookstore/configurations/JestClientConfig.java
@@ -3,17 +3,21 @@ package aditya.netent.assignment.bookstore.configurations;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JestClientConfig {
 
+    @Value("${es.host}")
+    private String esHost;
+
     @Bean
     public JestClient jestClient(){
         JestClientFactory factory = new JestClientFactory();
         factory.setHttpClientConfig(
-            new HttpClientConfig.Builder("https://pmiqtborz:r8xxa4glyn@bookstore-7320592115.ap-southeast-2.bonsaisearch.net:443")
+            new HttpClientConfig.Builder(esHost)
                                 .multiThreaded(true)
                                 .defaultMaxTotalConnectionPerRoute(2)
                                 .maxTotalConnection(10)
